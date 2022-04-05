d72ebd1dc6a409e53c5b2f153679812e1bac0c75
Evan Bubniak <evan.bubniak@gmail.com>
2022-04-04 20:05:51 -0400

    add quotation mark

--- remove_chars.py
+++ remove_chars.py
@@ -1,4 +1,4 @@
-chars_to_remove = [",", ".", "?", "«", "»","「","」","(",")"]
+chars_to_remove = [",", ".", "?", "«", "»","「","」","(",")", "\""]
 def remove_chars(title):
     for char_to_remove in chars_to_remove:
         title = title.replace(char_to_remove, "")
